38ba9d3cf5fac3f1c4669609456c09daef21bbe3
Wing Leong Au Yeung <jacky2004001@gmail.com>
2022-12-08 12:53:31 -0800

    Refactor

--- content-script.js
+++ content-script.js
@@ -3,194 +3,270 @@ const gameChat = document.getElementById('game-chat');
 const inputForm = gameChat.firstChild.childNodes[2];
 const inputChatReal = inputForm.firstChild;
 const inputChatFake = inputChatReal.cloneNode(true);
+const submitBtn = document.createElement('button');
 const config = { attributes: true, childList: true, subtree: true };
 
-let words = {};
+class Chat {
+  /** @type {Element} */
+  #realChat
+  /** @type {Element} */
+  #fakeChat
+  /** @type {Element} */
+  #submitBtn
 
-const callback = function (mutations_list, observer) {
-  displaySolutions(findSolutions(unwrapClue(currentWord)));
-};
-
-const observer = new MutationObserver(callback);
-
-const suggContainer = document.createElement('div');
-
-let currentSolutions = [];
-
-/**
- * Turns a DOM node that contains the hint into a string
- * that is interruptable by the program.
- * @param {Node} node
- * @returns {String} A clue interruptable by findSolutions()
- */
-function unwrapClue(node) {
-  const hints = node.childNodes[2].firstChild.childNodes;
-  let clue = '';
-  for (let i = 0; i < hints.length - 1; i++) {
-    clue += hints.item(i).textContent;
+  /**
+   * @param {Element} realChatNode 
+   * @param {Element} fakeChatNode 
+   * @param {Element} submitBtnNode 
+   */
+  constructor(realChatNode, fakeChatNode, submitBtnNode) {
+    this.#realChat = realChatNode;
+    this.#fakeChat = fakeChatNode;
+    this.#submitBtn = submitBtnNode;
   }
-  return clue;
+
+  /**
+   * Writes a message to the chat.
+   * @param {String} text Message
+   */
+  write(text) {
+    this.#realChat.value = text;
+    this.#fakeChat.value = text;
+  }
+
+  /**
+   * Clears the chat.
+   */
+  clear() {
+    this.write('');
+  }
+
+  /**
+   * Submits the chat.
+   */
+  submit() {
+    this.#submitBtn.click();
+  }
 }
 
-/**
- * Finds a list of pontential answers by filtering out wrong guesses with a clue.
- * @param {String} clue 
- * @returns {String[]} A list of potential answers
- */
-function findSolutions(clue) {
-  const numWords = clue.split(' ').length;
-  const lens = clue.split(' ').map(word => word.length);
-  clue = clue.replaceAll(' ', '').replaceAll('-', '').toLowerCase();
-  if (words[numWords] !== undefined && words[numWords][clue.length] !== undefined) {
-    let guesses = words[numWords][clue.length];
-    let letterPos = 0;
+class Bot {
+  /** @type {Chat} */
+  #chat;
+  /** @type {Element} */
+  #suggContainer;
+  /** @type {Element} */
+  #words;
+  /** @type {Element} */
+  #inputForm;
+  /** @type {Element} */
+  #gameChat;
+  /** @type {Element} */
+  #indexMode;
+  /** @type {Element} */
+  #currentWord
+  #currentSolutions = [];
 
-    if (numWords > 1) {
-      guesses = guesses.filter(guess => guess.lens.every((e, i) => e === lens[i]));
+  /**
+   * @param {Element} realChatNode 
+   * @param {Element} fakeChatNode 
+   * @param {Element} submitBtn 
+   * @param {Element} inputForm 
+   * @param {Element} gameChat 
+   */
+  constructor(realChatNode, fakeChatNode, submitBtn, inputForm, gameChat, currentWord) {
+    this.#chat = new Chat(realChatNode, fakeChatNode, submitBtn);
+    this.#inputForm = inputForm;
+    this.#gameChat = gameChat;
+    this.#indexMode = true;
+    this.#currentWord = currentWord;
+
+    const callback = (mutations_list, observer, b=this) => {
+      b.displaySolutions(b.findSolutions(b.getCurrentWord()));
+    };
+    
+    const observer = new MutationObserver(callback);
+    this.#suggContainer = document.createElement('div');
+    
+    fetch('https://www.wlay.me/static/json/words.json')
+    .then(res => res.json())
+    .then(data => this.#words = data)
+    .catch(e => console.log(e));
+  
+    observer.observe(currentWord, config);
+    this.#suggContainer.setAttribute('class', 'suggestions');
+    this.#inputForm.append(this.#suggContainer);
+    this.#inputForm.append(submitBtn);
+    
+    realChatNode.after(inputChatFake);
+    realChatNode.style.visibility = 'hidden';
+    
+    fakeChatNode.addEventListener('input', (e) => {
+      const guess = fakeChatNode.value.toLowerCase();
+      const possibilities = this.findSolutions(this.getCurrentWord());
+      const newPossibilities = [];
+    
+      if (!isNaN(parseInt(guess))) return;
+    
+      if (possibilities.length > 0) {
+        for (const possibility of possibilities) {
+          if (possibility.word.toLowerCase().includes(guess)) {
+            newPossibilities.push(possibility);
+          }
+        }
+      }
+    
+      this.displaySolutions(newPossibilities, false);
+    });
+    
+    fakeChatNode.addEventListener('keypress', (e) => {
+      if (e.code === 'Enter') {
+        const idx = parseInt(inputChatFake.value);
+        if (this.#indexMode && !isNaN(idx) && idx >= 0 
+          && idx <= this.#suggContainer.childNodes.length - 1) {
+          fakeChatNode.value = this.#suggContainer.childNodes[idx].getAttribute('word');
+          this.removeSolution(this.#suggContainer.childNodes[idx]);
+        }
+        this.#chat.write(inputChatFake.value);
+        this.#chat.submit();
+      }
+    });
+    
+    document.addEventListener('keydown', (e) => {
+      if (e.ctrlKey || e.metaKey) {
+        e.preventDefault();
+        fakeChatNode.focus();
+        this.#chat.clear();
+        this.displaySolutions(this.#currentSolutions);
+      }
+      if (e.code === 'Tab') {
+        e.preventDefault();
+        const solutions = this.#suggContainer.childNodes;
+        let node = document.activeElement;
+        if (solutions.length === 0) return;
+        if (node.className !== 'solution' || node.nextSibling === null) {
+          node = solutions[0];
+        } else {
+          node = node.nextSibling;
+        }
+        node.focus();
+      }
+    });
+    
+    this.#gameChat.addEventListener('submit', () => {
+      this.displaySolutions(this.#currentSolutions);
+    });
+  }
+  
+  /**
+   * Turns a DOM node that contains the hint into a string
+   * that is interruptable by the program.
+   * @param {Node} node
+   * @returns {String} A clue interruptable by findSolutions()
+   */
+  unwrapClue(node) {
+    const hints = node.childNodes[2].firstChild.childNodes;
+    let clue = '';
+    for (let i = 0; i < hints.length - 1; i++) {
+      clue += hints.item(i).textContent;
     }
+    return clue;
+  }
 
-    while (clue.length !== 0) {
-      let letter = '';
-      while (clue.charAt(0) === '_') {
+  /**
+   * Finds a list of pontential answers by filtering out wrong guesses with a clue.
+   * @param {String} clue 
+   * @returns {String[]} A list of potential answers
+   */
+  findSolutions(clue) {
+    const numWords = clue.split(' ').length;
+    const lens = clue.split(' ').map(word => word.length);
+    clue = clue.replaceAll(' ', '').replaceAll('-', '').toLowerCase();
+    if (this.#words[numWords] !== undefined && this.#words[numWords][clue.length] !== undefined) {
+      let guesses = this.#words[numWords][clue.length];
+      let letterPos = 0;
+
+      if (numWords > 1) {
+        guesses = guesses.filter(guess => guess.lens.every((e, i) => e === lens[i]));
+      }
+
+      while (clue.length !== 0) {
+        let letter = '';
+        while (clue.charAt(0) === '_') {
+          clue = clue.substring(1);
+          letterPos++;
+        }
+        letter = clue.charAt(0).toLowerCase();
+        if (letter !== '') {
+          guesses = guesses.filter((guess) => guess.letters.charAt(letterPos) === letter);
+        }
         clue = clue.substring(1);
         letterPos++;
       }
-      letter = clue.charAt(0).toLowerCase();
-      if (letter !== '') {
-        guesses = guesses.filter((guess) => guess.letters.charAt(letterPos) === letter);
-      }
-      clue = clue.substring(1);
-      letterPos++;
-    }
-    return guesses;
-  } else {
-    return [];
-  }
-}
-
-/**
- * Displays the list of potential solutions under the chat bar.
- * @param {String[]} solutions The list of potential solutions
- * @param {Boolean} replace True if the new answers should replace
- * all of the old ones
- */
-function displaySolutions(solutions, replace = true) {
-  let i = 0;
-  suggContainer.innerHTML = '';
-  for (const word of solutions) {
-    const choice = document.createElement('div');
-    const btn = document.createElement('button');
-    const index = document.createElement('span');
-    const submit = () => {
-      inputChatReal.value = word.word;
-      inputChatFake.value = word.word;
-      btn.click();
-      removeSolution(choice);
-    };
-    btn.innerHTML = word.word;
-    btn.setAttribute('type', 'submit');
-    index.innerHTML = i;
-    choice.innerHTML = word.word;
-    choice.addEventListener('mousedown', submit);
-    choice.addEventListener('keypress', e => {
-      if (e.code === 'Enter') submit();
-    });
-    choice.setAttribute('class', 'solution');
-    choice.setAttribute('tabindex', '-1');
-    choice.setAttribute('word', word.word);
-    choice.addEventListener('focus', (e) => {
-      const value = word.word;
-      inputChatReal.value = value;
-      inputChatFake.value = word.word;
-    });
-    choice.append(index);
-    choice.append(btn);
-    suggContainer.append(choice);
-    i++;
-  }
-  if (replace) {
-    currentSolutions = solutions;
-  }
-}
-
-/**
- * Remove a possible solution from the suggestion list.
- * @param {Element} solutionNode 
- */
-function removeSolution(solutionNode) {
-  const word = solutionNode.getAttribute('word');
-  for (let i = 0; i < currentSolutions.length; i++) {
-    if (currentSolutions[i].word === word) {
-      currentSolutions.splice(i, 1);
-    }
-  }
-  setTimeout(() => { solutionNode.remove() }, 50);
-}
-
-fetch('https://www.wlay.me/static/json/words.json')
-  .then(res => res.json())
-  .then(data => words = data)
-  .catch(e => console.log(e));
-
-observer.observe(currentWord, config);
-suggContainer.setAttribute('class', 'suggestions');
-inputForm.append(suggContainer);
-
-inputChatReal.after(inputChatFake);
-inputChatReal.style.visibility = 'hidden';
-
-inputChatFake.addEventListener('input', (e) => {
-  const guess = inputChatFake.value.toLowerCase();
-  const possibilities = findSolutions(unwrapClue(currentWord));
-  const newPossibilities = [];
-
-  if (!isNaN(parseInt(guess))) return;
-
-  if (possibilities.length > 0) {
-    for (const possibility of possibilities) {
-      if (possibility.word.toLowerCase().includes(guess)) {
-        newPossibilities.push(possibility);
-      }
-    }
-  }
-
-  displaySolutions(newPossibilities, false);
-});
-
-inputChatFake.addEventListener('keypress', (e) => {
-  if (e.code === 'Enter') {
-    const idx = parseInt(inputChatFake.value);
-    if (!isNaN(idx) && idx >= 0 && idx <= suggContainer.childNodes.length - 1) {
-      inputChatFake.value = suggContainer.childNodes[idx].getAttribute('word');
-      removeSolution(suggContainer.childNodes[idx]);
-    }
-    inputChatReal.value = inputChatFake.value;
-    inputChatReal.click();
-  }
-});
-
-document.addEventListener('keydown', (e) => {
-  if (e.ctrlKey || e.metaKey) {
-    e.preventDefault();
-    inputChatFake.focus();
-    inputChatFake.value = '';
-    inputChatReal.value = '';
-    displaySolutions(currentSolutions);
-  }
-  if (e.code === 'Tab') {
-    e.preventDefault();
-    const solutions = suggContainer.childNodes;
-    let node = document.activeElement;
-    if (solutions.length === 0) return;
-    if (node.className !== 'solution' || node.nextSibling === null) {
-      node = solutions[0];
+      return guesses;
     } else {
-      node = node.nextSibling;
+      return [];
     }
-    node.focus();
   }
-});
 
-gameChat.addEventListener('submit', () => {
-  displaySolutions(currentSolutions);
-});
+  /**
+   * Displays the list of potential solutions under the chat bar.
+   * @param {String[]} solutions The list of potential solutions
+   * @param {Boolean} replace True if the new answers should replace
+   * all of the old ones
+   */
+  displaySolutions(solutions, replace = true) {
+    let i = 0;
+    this.#suggContainer.innerHTML = '';
+    for (const word of solutions) {
+      const choice = document.createElement('div');
+      const index = document.createElement('span');
+      const submit = () => {
+        this.#chat.write(word.word);
+        this.removeSolution(choice);
+        this.#chat.submit();
+      };
+      index.innerHTML = i;
+      choice.innerHTML = word.word;
+      choice.addEventListener('mousedown', submit);
+      choice.addEventListener('keypress', e => {
+        if (e.code === 'Enter') submit();
+      });
+      choice.setAttribute('class', 'solution');
+      choice.setAttribute('tabindex', '-1');
+      choice.setAttribute('word', word.word);
+      choice.addEventListener('focus', (e) => {
+        this.#chat.write(word.word);
+      });
+      choice.append(index);
+      this.#suggContainer.append(choice);
+      i++;
+    }
+    if (replace) {
+      this.#currentSolutions = solutions;
+    }
+  }
+
+  /**
+   * Remove a possible solution from the suggestion list.
+   * @param {Element} solutionNode 
+   */
+  removeSolution(solutionNode) {
+    const word = solutionNode.getAttribute('word');
+    for (let i = 0; i < this.#currentSolutions.length; i++) {
+      if (this.#currentSolutions[i].word === word) {
+        this.#currentSolutions.splice(i, 1);
+      }
+    }
+    setTimeout(() => { solutionNode.remove(); }, 50);
+  }
+
+  /**
+   * @returns {String} A clue
+   */
+  getCurrentWord() {
+    return this.unwrapClue(currentWord);
+  }
+}
+
+const bot = new Bot(inputChatReal, inputChatFake, submitBtn, inputForm,
+  gameChat, currentWord);
